8b5685e2ce417db2df25ab82fb8cb70bf49ddb84
ljc <dlwlcjf558@naver.com>
2022-08-24 16:44:54 +0900

    order dto, dao 추가

---
+++ 1/src/main/java/vo/ordDTO.java
@@ -0,0 +1,59 @@
+package vo;
+
+import java.sql.Date;
+
+//CREATE TABLE ord(
+//		order_num int PRIMARY KEY,
+//		order_user_email VARCHAR(50) NOT NULL,
+//		order_date DATE NOT NULL,
+//		order_status VARCHAR(20) default '결제완료',
+//		order_price int NOT NULL,
+//		constraint ord_email_fk foreign key(order_user_email) references user(user_email)
+//	);
+
+//INSERT INTO ord value(1,'a@naver.com',now(),default,50000);
+
+public class ordDTO {
+	private int order_num;
+	private Date order_date;
+	private String order_status;
+	private String order_user_email;
+	private int order_price;
+	
+	public int getOrder_num() {
+		return order_num;
+	}
+	public void setOrder_num(int order_num) {
+		this.order_num = order_num;
+	}
+	public Date getOrder_date() {
+		return order_date;
+	}
+	public void setOrder_date(Date order_date) {
+		this.order_date = order_date;
+	}
+	public String getOrder_status() {
+		return order_status;
+	}
+	public void setOrder_status(String order_status) {
+		this.order_status = order_status;
+	}
+	public String getOrder_user_email() {
+		return order_user_email;
+	}
+	public void setOrder_user_email(String order_user_email) {
+		this.order_user_email = order_user_email;
+	}
+	public int getOrder_price() {
+		return order_price;
+	}
+	public void setOrder_price(int order_price) {
+		this.order_price = order_price;
+	}
+	
+	
+	
+
+	
+	
+}
---
+++ 1/src/main/java/vo/ordDetailDTO.java
@@ -0,0 +1,70 @@
+package vo;
+
+import java.sql.Date;
+
+//CREATE TABLE ordDetail(
+//		order_num int NOT NULL,
+//		order_pd_num int NOT NULL,
+//		order_quan int NOT NULL,
+//		order_pd_price int NOT NULL,
+//		constraint ord_num_fk foreign key(order_num) references ord(order_num),
+//		constraint ord_pdn_fk foreign key(order_pd_num) references product(pd_num)
+//	);
+
+//insert into ordDetail value(1,5,5,20000);
+
+public class ordDetailDTO {
+	private int order_num;
+	private int order_pd_num;
+	private int order_quan;
+	private int order_pd_price;
+	
+	//조인용 멤버
+	private String pd_file;
+	private String pd_subject;
+
+	public int getOrder_num() {
+		return order_num;
+	}
+	public void setOrder_num(int order_num) {
+		this.order_num = order_num;
+	}
+	public int getOrder_pd_num() {
+		return order_pd_num;
+	}
+	public void setOrder_pd_num(int order_pd_num) {
+		this.order_pd_num = order_pd_num;
+	}
+	public int getOrder_quan() {
+		return order_quan;
+	}
+	public void setOrder_quan(int order_quan) {
+		this.order_quan = order_quan;
+	}
+	public int getOrder_pd_price() {
+		return order_pd_price;
+	}
+	public void setOrder_pd_price(int order_pd_price) {
+		this.order_pd_price = order_pd_price;
+	}
+
+
+	
+	// 조인용 get, set
+	public String getPd_file() {
+		return pd_file;
+	}
+	public void setPd_file(String pd_file) {
+		this.pd_file = pd_file;
+	}
+	public String getPd_subject() {
+		return pd_subject;
+	}
+	public void setPd_subject(String pd_subject) {
+		this.pd_subject = pd_subject;
+	}
+	
+
+	
+	
+}
